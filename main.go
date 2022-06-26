47c74be00bbabe1b6927bf75f283fa09d4bb1650
psihachina <psihachina@gmail.com>
2022-06-26 15:09:28 +0300

    add IsSsh function
    IsSsh check if an input value is a ssh url or not

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package path_parser
 
 import (
+	"regexp"
 	"strings"
 )
+
+type Output struct {
+	Protocols []string
+	Protocol  string
+	Port      int
+	Resource  string
+	User      string
+	Pathname  string
+	Hash      string
+	Href      string
+	Query     string
+}
+
+// IsSsh check if an input value is a ssh url or not
+func IsSsh(input string) bool {
+	protocols := Protocols(input)
+	input = input[strings.Index(input, "://")+3:]
+
+	for _, p := range protocols {
+		if strings.HasPrefix(p, "ssh") || strings.HasPrefix(p, "rsync") {
+			return true
+		}
+	}
+
+	var re = regexp.MustCompile(`\.([a-zA-Z\\d]+):(\d+)`)
+	return !re.Match([]byte(input)) && strings.Index(input, "@") < strings.Index(input, ":")
+}
 
 // Protocols returns the protocols of an input url.
 func Protocols(input string) []string {
--- main_test.go
+++ main_test.go
@@ -1,7 +1,8 @@
 package path_parser
 
 import (
-	"reflect"
+	"github.com/stretchr/testify/require"
+	"strconv"
 	"testing"
 )
 
@@ -18,9 +19,55 @@ func TestProtocols(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			if !reflect.DeepEqual(Protocols(test.input), test.expected) {
-				t.Fail()
-			}
+			require.Equal(t, Protocols(test.input), test.expected)
+		})
+	}
+}
+
+func TestIsSsh(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		// Secure Shell Transport Protocol (SSH)
+		{"ssh://user@host.xz:port/path/to/repo.git/", true},
+		{"ssh://user@host.xz/path/to/repo.git/", true},
+		{"ssh://host.xz:port/path/to/repo.git/", true},
+		{"ssh://host.xz/path/to/repo.git/", true},
+		{"ssh://user@host.xz/path/to/repo.git/", true},
+		{"ssh://host.xz/path/to/repo.git/", true},
+		{"ssh://user@host.xz/~user/path/to/repo.git/", true},
+		{"ssh://host.xz/~user/path/to/repo.git/", true},
+		{"ssh://user@host.xz/~/path/to/repo.git", true},
+		{"ssh://host.xz/~/path/to/repo.git", true},
+		{"user@host.xz:/path/to/repo.git/", true},
+		{"user@host.xz:~user/path/to/repo.git/", true},
+		{"user@host.xz:path/to/repo.git", true},
+		{"host.xz:/path/to/repo.git/", true},
+		{"host.xz:path/to/repo.git", true},
+		{"host.xz:~user/path/to/repo.git/", true},
+		{"rsync://host.xz/path/to/repo.git/", true},
+
+		// Git Transport Protocol
+		{"git://host.xz/path/to/repo.git/", false},
+		{"git://host.xz/~user/path/to/repo.git/", false},
+
+		// HTTP/S Transport Protocol
+		{"http://host.xz/path/to/repo.git/", false},
+		{"https://host.xz/path/to/repo.git/", false},
+		{"http://host.xz:8000/path/to/repo.git/", false},
+		{"https://host.xz:8000/path/to/repo.git/", false},
+		// Local (Filesystem) Transport Protocol
+		{"/path/to/repo.git/", false},
+		{"path/to/repo.git/", false},
+		{"~/path/to/repo.git", false},
+		{"file:///path/to/repo.git/", false},
+		{"file://~/path/to/repo.git/", false},
+	}
+
+	for n, test := range tests {
+		t.Run(strconv.Itoa(n), func(t *testing.T) {
+			require.Equal(t, test.expected, IsSsh(test.input))
 		})
 	}
 }
